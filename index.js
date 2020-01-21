85eb8354f8dabeaa7135884035bc1449c0a7abd6
Paulo Correa <pauloyaco@gmail.com>
2020-01-20 23:44:24 -0300

    add swagger-ui-express docs

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express')
-
 const servidor = express();
+const swaggerUi = require('swagger-ui-express');
+var options = {
+    explorer: true,
+    swaggerOptions: {
+      urls: [
+        {
+          url: 'http://petstore.swagger.io/v2/swagger.json',
+          name: 'Spec1'
+        },
+        {
+          url: 'http://petstore.swagger.io/v2/swagger.json',
+          name: 'Spec2'
+        }
+      ]
+    }
+  }
+   
+  servidor.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'dbuser',
+      password : 'dbpass',
+      database : 'dbdatabase'
+    }
+  });
 
 servidor.get('/', (requisicao, resposta) => {
     return resposta.json({ 
